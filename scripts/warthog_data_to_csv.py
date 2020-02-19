d9af0a4660cfbb3696971537ce49786277fc3705
Akhil <akhil.nagariya@gmail.com>
2020-02-18 20:41:25 -0600

    added 15th feb data

--- scripts/warthog_data_to_csv.py
+++ scripts/warthog_data_to_csv.py
@@ -1,14 +1,15 @@
 import rospy
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 import time
 import csv
 
 class DataLog:
 	def __init__(self):
 		rospy.Subscriber('/warthog_velocity_controller/odom', Odometry, self.odomcb)
-		rospy.Subscriber('/gcartodom', Odometry, self.cmdcb)
-		self.odom_data_file = open('warthog_odom_run.csv', 'w')
-                self.cmd_data_file = open('warthog_cmd_run.csv', 'w')
+		rospy.Subscriber('/warthog_velocity_controller/cmd_vel', Twist, self.cmdcb)
+		self.odom_data_file = open('warthog_odom_run_15_feb.csv', 'w')
+                self.cmd_data_file = open('warthog_cmd_run_15_feb.csv', 'w')
 		self.field_odom = ['v','w','t']
 		self.field_cmd = ['v_c','w_c','t_c']
 		self.odom_writer = csv.DictWriter(self.odom_data_file, self.field_odom)
@@ -26,9 +27,9 @@ class DataLog:
 		self.odom_num+=1
 		print('odom_msg ', self.odom_num)
         def cmdcb(self, msg):
-		v = msg.twist.twist.linear.x
-		w = msg.twist.twist.angular.z
-		dt = (msg.header.stamp - self.init_time).to_sec()
+		v = msg.linear.x
+		w = msg.angular.z
+		dt = (rospy.Time.now() - self.init_time).to_sec()
 		self.cmd_writer.writerow({'v_c': v, 'w_c': w, 't_c': dt})
 		self.cmd_num+=1
 		print('cmd_msg ', self.cmd_num)
